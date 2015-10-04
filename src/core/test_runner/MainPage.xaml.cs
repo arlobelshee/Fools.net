f92109ff3770e23ee76bb838fdc16e45a24d0ffa
Arlo Belshee <arlo.belshee@live.com>
2015-10-04 08:29:34 -0700

    Created a universal windows 10 app as a test runner. Currently a blank app that can be deployed and run.

---
+++ src/core/test_runner/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using JetBrains.Annotations;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace test_runner
+{
+	/// <summary>
+	///    An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		[NotNull] private string _parameter;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnNavigatedTo([NotNull] NavigationEventArgs e)
+		{
+			base.OnNavigatedTo(e);
+			_parameter = ((string) e.Parameter) ?? string.Empty;
+		}
+	}
+}
