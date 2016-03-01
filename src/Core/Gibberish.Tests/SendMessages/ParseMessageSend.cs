69e92be24c15aee95fdebf09f59634c8ea3d8c36
Arlo Belshee <arlo.belshee@live.com>
2016-02-29 21:02:06 -0800

    Adding in stub tests for the various operations I'm going to need to recognize / parse in order to handle thunk bodies (enough to get Hello World with exit to work).

---
+++ src/Core/Gibberish.Tests/SendMessages/ParseMessageSend.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Gibberish.Tests.SendMessages
+{
+	[TestFixture]
+	public class ParseMessageSend
+	{
+		[Test, Ignore("Not started")]
+		public void ShouldRecognizeCreateNewMessageInstance() { }
+
+		[Test, Ignore("Not started")]
+		public void ShouldRecognizeSetMessageField() { }
+
+		[Test, Ignore("Not started")]
+		public void ShouldRecognizeBasicMessageSend() { }
+	}
+}
---
+++ src/Core/Gibberish.Tests/SendMessages/ParseVariableManipulations.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Gibberish.Tests.SendMessages
+{
+	public class ParseVariableManipulations
+	{
+		[Test, Ignore("Not started")]
+		public void ShouldRecognizePushAMailingAddress() { }
+
+		[Test, Ignore("Not started")]
+		public void ShouldRecognizePushAConstantInteger() { }
+
+		[Test, Ignore("Not started")]
+		public void ShouldRecognizePushEntryFromStringTable() { }
+	}
+}
