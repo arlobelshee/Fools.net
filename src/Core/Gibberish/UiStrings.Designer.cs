5abcea67f9d69d6d27da6726a19d53c20ee02516
Arlo Belshee <arlo.belshee@live.com>
2016-05-24 08:25:28 -0700

    **broken** resolving more references correctly via NuGet. But IronMeta doesn't work on UWP, so I'll need to remove that dep - which I can do. The current parser doesn't really take advantage of PEGs anyway.

--- src/Core/Gibberish/UiStrings.Designer.cs
+++ src/Core/Gibberish/UiStrings.Designer.cs
@@ -8,6 +8,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.Reflection;
+
 namespace Gibberish {
     using System;
     
@@ -39,7 +41,7 @@ namespace Gibberish {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gibberish.UiStrings", typeof(UiStrings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gibberish.UiStrings", typeof(UiStrings).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
